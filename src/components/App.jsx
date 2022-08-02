8b52c57728b5e9ccdda245e3bade895dce036d3f
GudymaVit <gudymavit1988@gmail.com>
2022-08-02 21:15:37 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,17 @@
-import { useState, useEffect } from 'react';
 import ContactForm from './contactForm';
 import Filter from './filter';
 import ContactList from './contactList';
 import styles from '../components/contactForm/contactForm.module.css';
 
-const initialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-const storegeContacts = JSON.parse(localStorage.getItem('contacts'));
-
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return storegeContacts ? storegeContacts : initialState;
-  });
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = newContact => {
-    const normalizedContactsName = contacts.map(contact =>
-      contact.name.toLowerCase()
-    );
-    const normalizedNewContact = newContact.name.toLowerCase();
-
-    if (normalizedContactsName.includes(normalizedNewContact)) {
-      alert(`${newContact.name} is already in contacts.`);
-    } else {
-      setContacts(state => [newContact, ...state]);
-    }
-  };
-
-  const deleteContact = id => {
-    setContacts(state => state.filter(contact => contact.id !== id));
-  };
-
-  const handleInputChenge = e => {
-    const { value } = e.target;
-    setFilter(value);
-  };
-
-  const filterContacts = () => {
-    const normalizedText = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedText)
-    );
-  };
-
-  const filtered = filterContacts();
   return (
     <>
       <div className={styles.container}>
         <h1>Phonebook</h1>
-        <ContactForm addContact={addContact} />
-
+        <ContactForm />
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={handleInputChenge} />
-        <ContactList contacts={filtered} onDelete={deleteContact} />
+        <Filter />
+        <ContactList />
       </div>
     </>
   );
---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactSlice } from './contactSlice';
+
+const rootReducer = combineReducers({
+    contacts: contactSlice.reducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+});
+const persistor = persistStore(store);
+
+export { store, persistor};
